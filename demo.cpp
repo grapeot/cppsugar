816fbd491170b2d265d6c8bd334f4e7a5d418203
Yan Wang <grapeot@gmail.com>
2014-04-29 17:19:10 -0700

    update the demo cpp file

--- demo.cpp
+++ demo.cpp
@@ -9,12 +9,17 @@ int main() {
     auto vec = Range(20);
     // select the even numbers
     vec = Filter<int>(vec, [](int v) { return v % 2 == 0; });
-    // compute the square
-    vec = Map<int, int>(vec, [](int v) { return v * v; });
-    // output
-    for (auto v : vec) {
-        cout << v << endl;
-    }
-
+    // compute x * (20 - x)
+    vec = Map<int, int>(vec, [](int v) { return v * (20 - v); });
+    for (auto v : vec) cout << v << endl;
+    // unique!
+    cout << endl;
+    for (auto v : Unique(vec)) cout << v << endl;
+    // min/max/sum
+    cout << endl
+        << "The minimum element is: " << Min(vec) << endl
+        << "The sum is: " << Sum(vec) << endl
+        << "The max of square is: " << Max<int, int>(vec, [](int x) { return x * x; }) << endl;
+    
     return 0;
 }
